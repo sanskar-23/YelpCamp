d7f2147cf5cf0aedf7beead5d4e76427e639b43a
Sanskar-23 <skapoor_be20@thapar.edu>
2022-06-18 01:18:24 +0530

    Completed the Controller Function

--- seeds/index.js
+++ seeds/index.js
@@ -21,6 +21,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '62ac76982934ce5b4d3a8e0d',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
